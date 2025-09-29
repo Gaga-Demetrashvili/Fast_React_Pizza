9feaaca362bc88aaea10b3de7557b1178ffcdd7d
Gaga Demetrashvili <gagademetrashvili@credo.ge>
2025-09-30 01:54:45 +0400

    Complete tailwind Crash Course

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",],
+  // Adding int 'theme' overrwrites every class in that category
   theme: {
-    extend: {},
+    fontFamily: {
+      // tailwind automatically injects sans font in our html
+      // we can override that sans font with font of our liking
+      sans: 'Roboto Mono, monospace'
+    },
+
+    // if you want to keep tailwind classes, but also to
+    // extend it, place your classes here
+    extend: {
+      colors: {
+        pizza: '#123456',
+      },
+      height: {
+        screen: '100dvh' // more modern approach then dvh (dynamic viewport height units)
+      }
+    },
   },
   plugins: [],
 }
